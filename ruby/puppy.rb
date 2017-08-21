3de4c3d8594c9601ad9ab94514c90fafb3daef79
Lina Zoo <linazooper@gmail.com>
2017-08-21 16:17:15 -0400

    add puppy class instance

---
+++ ruby/puppy.rb
@@ -0,0 +1,14 @@
+#SPECIES ------------------------
+Dogger Pupper
+
+CHARACTERISTICS ----------------
+leg count: varies
+cute: true
+fur: true
+Name: varies
+Color: varies
+
+BEHAVIOR -----------------------
+Bark
+Run
+Chase
