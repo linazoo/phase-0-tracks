03f18917b7e1c902517d61f9daf82f6219259e53
Lina Zoo <linazooper@gmail.com>
2017-08-30 15:45:26 -0400

    add solutions for javascript practice

---
+++ js/explore.js
@@ -0,0 +1,49 @@
+// foward
+function countToTen() {
+  for(var i = 1 ; i <= 10; i++){
+    console.log(i);
+  }
+}
+
+countToTen();
+
+// backward
+function countToTen() {
+  for(var i = 10 ; i >= 0; i--){
+    console.log(i);
+  }
+}
+
+countToTen();
+
+
+//function takes a string as a parameter and reverses the string reverse("hello") => "olleh"
+//split method to split the string into an array of substrings
+//reverse method by moving last to first
+//joining
+
+
+function reverseString(string) {
+  //Use the split method to return a new array
+  var reverseString = string.split("");
+  //splitstring = "hello.split("");
+  var reverseArray = splitstring.reverse();
+  var joinArray = reverseArray.join("");
+  return joinArray;
+}
+reverseString("hello");
+
+
+function reverseString(str) {
+    return str.split("").reverse().join("");
+}
+  console.log(reverseString("hello"));
+
+// print the variable if some silly condition is true.
+
+if (reverseString === 1==1) {
+  console.log(reverseString);
+}
+  else {
+  console.log('wut?')
+}
